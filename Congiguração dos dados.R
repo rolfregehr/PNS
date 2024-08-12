f70030ddae0ff5b38912e0f0c6ec8fe2daa436ff
Rolf <rolf.regehr@gmail.com>
2024-08-12 18:32:00 -0300

    Novos ajustes

--- Congiguração dos dados.R
+++ Congiguração dos dados.R
@@ -23,13 +23,12 @@ options(dplyr.summarise.inform = FALSE) # exclui resumo
 options(message=FALSE, warning=FALSE) # exclui mensagens e alertas
 
   
-#  "ggcorrplot", "haven", "tinytex", "grid", "tidyverse", "ggpubr", "readxl", "shiny", "readr", 
-#               "scales", "dplyr", "corrplot", "devtools", "kableExtra", "knitr", "modelsummary", "forcats", 
-#               "sp", "ggplot2", "rgdal", "ineq", "convey", "pacman", "GISTools", "cowplot", "egg", 
-#               "classInt", "RColorBrewer", "ggmap", "mapproj", "plyr", "ggrepel", "gridExtra", "IC2"))
 
 
 # Coleta os dados a partir do pacote PNSIBGE e salva na pasta indicada
 dadosPNS_13 <- get_pns(year=2013, savedir = './dados_ibge/', selected = T) # design = T (padrão) / Morador selecionado
 dadosPNS_19 <- get_pns(year=2019, savedir = './dados_ibge/', selected = T) # design = T (padrão) / Morador selecionado
+
+
+
 

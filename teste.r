54fc38b2d3babc09e354d6f922e372f887b362ef
Rolf <rolf.regehr@gmail.com>
2024-08-12 18:33:28 -0300

    Limpando o Teste

--- teste.r
+++
@@ -1,4 +0,0 @@
-#  "ggcorrplot", "haven", "tinytex", "grid", "tidyverse", "ggpubr", "readxl", "shiny", "readr", 
-#               "scales", "dplyr", "corrplot", "devtools", "kableExtra", "knitr", "modelsummary", "forcats", 
-#               "sp", "ggplot2", "rgdal", "ineq", "convey", "pacman", "GISTools", "cowplot", "egg", 
-#               "classInt", "RColorBrewer", "ggmap", "mapproj", "plyr", "ggrepel", "gridExtra", "IC2"))
